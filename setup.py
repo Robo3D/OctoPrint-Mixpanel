9043853b8276ca01649febebf6d4ce356ccf2dc7
Victor E <victorevector@gmail.com>
2016-06-08 16:48:27 -0700

    added mixpanel depednency

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ plugin_url = "https://github.com/Robo3D/OctoPrint-Mixpanel"
 plugin_license = "TODO"
 
 # Any additional requirements besides OctoPrint should be listed here
-plugin_requires = []
+plugin_requires = ['mixpanel']
 
 ### --------------------------------------------------------------------------------------------------------------------
 ### More advanced options that you usually shouldn't have to touch follow after this point
