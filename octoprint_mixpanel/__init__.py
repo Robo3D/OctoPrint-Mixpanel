6c13be3bcb1674f8a820990295df14e0e6d2a74b
Victor E <victorevector@gmail.com>
2016-06-09 16:47:51 -0700

    moved all helper functions to init... cannot import from within import

--- octoprint_mixpanel/__init__.py
+++ octoprint_mixpanel/__init__.py
@@ -3,7 +3,44 @@ from __future__ import absolute_import
 
 import octoprint.plugin
 from mixpanel import Mixpanel
-from helper.octomix import generate_mixpanel_payload
+from datetime import datetime
+from octoprint.filemanager.analysis import GcodeAnalysisQueue 
+
+###HELPER FUNCTIONS###################
+def valid_event(event):
+	"""
+	Makes sure that event is valid for mixpanel. Returns booleans.
+	"""
+	return event in [
+		'Error',
+		'PrintStarted',
+		'PrintFailed',
+		'PrintDone',
+		'PrintCancelled',
+	]
+
+def volumetric_data():
+	d = GcodeAnalysisQueue()
+	return d['filament']['tool1']['volume']
+
+def generate_mixpanel_payload (event, payload):
+	"""
+	Helper function. generates and returns the mixpanel payload from the printer's event data.
+	"""
+	special_events = ['PrintStarted']
+	
+	if not valid_event(event):
+		return None
+
+	dt = datetime.utcnow()
+	payload['date'] = dt.date()
+	payload['time'] = dt.time()
+
+	if event in special_events:
+		payload['volume'] = volumetric_data()
+
+	return payload
+#################################
 
 class MixpanelPlugin(octoprint.plugin.StartupPlugin, octoprint.plugin.EventHandlerPlugin,octoprint.plugin.SettingsPlugin):
 	"""
